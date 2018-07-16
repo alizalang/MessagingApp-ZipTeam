3da6bb02b0be1f0c94d436650414712bb7c7276f
Roy <roym@MacBook-Pro-6.local>
2018-07-15 23:10:34 -0400

    added ionic-app

--- ZipTeamOrange/src/main/java/ZipTeamOrange/ZipTeamOrangeApplication.java
+++ ZipTeamOrange/src/main/java/ZipTeamOrange/ZipTeamOrangeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ZipTeamOrangeApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ZipTeamOrangeApplication.class, args);
 	}
--- ZipTeamOrange/src/main/java/ZipTeamOrange/user/UserController.java
+++ ZipTeamOrange/src/main/java/ZipTeamOrange/user/UserController.java
@@ -2,10 +2,7 @@ package ZipTeamOrange.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(path = "/user")
@@ -15,6 +12,7 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping(path="/add")
+    @CrossOrigin(origins = {"http://localhost:8100", "http://localhost:8080"})
     public @ResponseBody String addNewuser(@RequestParam String username, @RequestParam String email, @RequestParam String password){
         User user = new User(username, email, password);
 
