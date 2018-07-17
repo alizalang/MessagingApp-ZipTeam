9acff7e376d7745a0a459718b512532621a3cb36
Roy <roym@MacBook-Pro-6.local>
2018-07-17 01:10:12 -0400

    connected server to front-end

--- ZipTeamOrange/src/main/java/ZipTeamOrange/user/UserController.java
+++ ZipTeamOrange/src/main/java/ZipTeamOrange/user/UserController.java
@@ -11,14 +11,17 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping(path="/add")
-    @CrossOrigin(origins = {"http://localhost:8100", "http://localhost:8080"})
-    public @ResponseBody String addNewuser(@RequestParam String username, @RequestParam String email, @RequestParam String password){
-        User user = new User(username, email, password);
-
+    @PostMapping(path="/add")
+    @CrossOrigin(origins = {"http://localhost:8102"})
+    public @ResponseBody String addNewUser(@RequestBody User user){
         userRepository.save(user);
-
         return  "saved";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<User> getAllUser(){
+        //this returns a JSON or XML with users
+        return userRepository.findAll();
     }
 
 }
--- ZipTeamOrange/src/main/java/ZipTeamOrange/user/UserRepository.java
+++ ZipTeamOrange/src/main/java/ZipTeamOrange/user/UserRepository.java
@@ -1,13 +1,8 @@
 package ZipTeamOrange.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@RepositoryRestResource
-@CrossOrigin(origins = {"http://localhost:8100", "http://localhost:8080"})
+
 public interface UserRepository extends JpaRepository<User, Long>{
-
-
 
 }
